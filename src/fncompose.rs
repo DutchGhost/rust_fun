b91b6ba959414c94c72d28b94c08d0cb07a93185
DutchGhost <kasper199914@gmail.com>
2018-08-28 17:56:22 +0200

    changes

--- src/fncompose.rs
+++ src/fncompose.rs
@@ -1,13 +1,13 @@
 use std::marker::PhantomData;
 use super::*;
 
-struct FnCompose<T, O, F: Fn(T) -> O>
+struct FnCompose<T, O, F>
 {
     func: F,
     _items: PhantomData<(T, O)>,
 }
 
-impl <T, O, F: Fn(T) -> O> FnCompose<T, O, F> {
+impl <T, O, F> FnCompose<T, O, F> {
     const fn new(func: F) -> Self {
         Self {
             func,
@@ -15,9 +15,12 @@ impl <T, O, F: Fn(T) -> O> FnCompose<T, O, F> {
         }
     }
 
-    const fn then<OO, F2: Fn(O) -> OO>(self, func: F2) -> FnCompose<T, O, F> {
+    const fn then<OO>(self, func: impl Fn(O) -> OO) -> FnCompose<T, OO, impl Fn(T) -> OO>
+    where
+        F: Fn(T) -> O,
+    {
         Self {
-            func: compose(self.func, func) as _,
+            func: compose(self.func, func),
             _items: PhantomData
         }
     }
